5c83a9d6a0ba5157d3ee0fa7ff1f7d222e83d8ed
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2025-05-01 13:18:05 -0700

    added initial schema

---
+++ src/app/feed/page.tsx
@@ -0,0 +1,18 @@
+/*
+This is the feed page where users can see a list of potential matches.
+
+Should have a title: Doggy date suggestions
+
+should have a vertical scrollable list of potential match cards which include the following:
+
+- User name of the match
+- User profile photo (place holder for mvp)
+- Dog name
+- Dog breed
+- Dog age
+- Dog personality
+- Dog photo (place holder for mvp)
+
+Create a match component to display all of this info
+
+*/
